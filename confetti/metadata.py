2b0904e8530c7bb359770314f696d6df33953fdd
Rotem Yaari <vmalloc@gmail.com>
2013-03-12 11:55:28 +0200

    Fix metadata chaining

--- confetti/metadata.py
+++ confetti/metadata.py
@@ -5,4 +5,9 @@ class Metadata(object):
         super(Metadata, self).__init__()
         self.metadata = kwargs
     def __rfloordiv__(self, value):
+        if isinstance(value, Config):
+            if value.metadata is None:
+                value.metadata = {}
+            value.metadata.update(self.metadata)
+            return value
         return Config(value, metadata=self.metadata)
--- tests/test__metadata.py
+++ tests/test__metadata.py
@@ -7,8 +7,11 @@ class MetadataTest(TestCase):
         self.config = Config({
             "key1" : "value1" // Metadata(a=1, b=2),
             "key2" : "value2",
+            "key3" : 1 // Metadata(a=1) // Metadata(b=2),
             })
     def test__metadata_access(self):
         self.assertEquals(self.config.get_config("key1").metadata, {"a" : 1, "b" : 2})
     def test__metadata_access_through_parent(self):
         self.assertIsNone(self.config.get_config("key2").metadata)
+    def test__chained_metadata(self):
+        self.assertEquals(self.config.get_config("key3").metadata, {"a" : 1, "b" : 2})
